c4a00d83081a88d0c52b467fdb480795f124131b
acm-sp85 <acontell@gmail.com>
2021-03-21 18:58:19 -0400

    updated

--- index.js
+++ index.js
@@ -1,41 +1,20 @@
-// // Code your solutions in this file
-// for (let age = 30; age<40 ; age++){
-//     console.log(`I'm ${age} years old. Happy birthday to me!`);
-//     debugger;
-// }
-
-
-const gifts =['oso de peluche','drone','GIJOE'];
-const messageArray = [];
-
-
-
-// function wrapGifts(gifts){
-//     for (let i=0 ; i< gifts.length ; i++){
-
-//         console.log(`Wrapped ${gifts[i]} and added a bow!`);
-
-//     }
-//     return gifts;
-// }
-
-// wrapGifts(gifts);
-
-function writeCards(array,event){
-    for (let i=0 ; i < array.length ; i++){
-        debugger;
-        // console.log(`Thank you ${array[i]}, for the wonderful ${event} gift!`);
-        messageArray.push(`Thank you, ${array[i]}, for the wonderful ${event} gift!`);
+function writeCards(namesArray,event){
+    const newArray= [];
+    for (let i=0 ;i< namesArray.length ; i++){
+        newArray[i] = `Thank you, ${namesArray[i]}, for the wonderful ${event} gift!`;
+        
+        console.log(`Thank you, ${namesArray[i]}, for the wonderful ${event} gift!`)
     }
-    return messageArray;
-
+    return newArray;
 }
 
-function countDown(x){
-    
-    while (x >= 0){
-        console.log(x);
-        x--;
-    }
-}
+// writeCards(["Ada", "Brendan", "Ali"], "birthday"); 
 
+function countDown(integer){
+    let i = 0;
+    while (i<=integer){
+        console.log(i);
+        i++;
+    }
+
+}
